aa987b0ecdbbcfc9bbe3c4c0eeab54c71d4a95b7
Casey Brannon <cbrannon412@gmail.com>
2019-06-14 15:50:33 -0700

    templates

---
+++ main_app/urls.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render, redirect
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+ path('accounts/', include('django.contrib.auth.urls')),
+   
+]
