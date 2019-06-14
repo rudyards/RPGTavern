385e28bbce96bced3aa2d597f63510574fdb602b
Casey Brannon <cbrannon412@gmail.com>
2019-06-14 16:45:52 -0700

    profileRender

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
+ path('profile/', views.profile, name='profile_page'),   
  path('accounts/', include('django.contrib.auth.urls')),
+ 
    
 ]
