239e4ba83afecea4d7baa0bdc86dc02816e8ee77
Lyndsay Ramberg <lyndsayramberg@Lyndsays-MacBook-Pro.local>
2019-06-15 13:15:26 -0700

    home page and profile routes

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
- path('accounts/', include('django.contrib.auth.urls')),
-   
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('', views.home, name='home'),
+    path('profile/', views.profile, name='profile_page'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
 from .models import Profile
 from django.contrib.auth import login
+
+
 # Create your views here.
+def home(request):
+  return render(request, 'home.html')
+
+def profile(request):
+    return render(request, 'profile.html')
