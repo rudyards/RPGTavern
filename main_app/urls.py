9a2997c60686c84bc9e30c6e06b133eac4e033c3
Casey Brannon <cbrannon412@gmail.com>
2019-06-16 11:29:19 -0700

    Login/logout/signup

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,10 +1,9 @@
-from django.shortcuts import render, redirect
 from django.urls import path, include
 from . import views
 
 urlpatterns = [
  path('profile/', views.profile, name='profile_page'),   
  path('accounts/', include('django.contrib.auth.urls')),
- 
+ path('accounts/signup', views.signup, name='signup'),
    
 ]
