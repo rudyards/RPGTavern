d1ac0cfbf8aa6756195348f8cf90969b78600e70
rudyards <rudy@mightycheese.com>
2019-06-18 14:14:52 -0700

    expanded admin page

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from .models import Game, Character, Meeting, Comment
+from .models import Game, Character, Meeting, Comment, Proflie_photo, Game_photo, Character_photo, Character_sheet_photo
 
 # Register your models here.
 admin.site.register(Game)
 admin.site.register(Character)
 admin.site.register(Meeting)
 admin.site.register(Comment)
+admin.site.register(Proflie_photo)
+admin.site.register(Game_photo)
+admin.site.register(Character_photo)
+admin.site.register(Character_sheet_photo)
