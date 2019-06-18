0079bfd300bf7423f23178e56c11c39d05823a93
MJ <MJ@Mohammads-MacBook-Pro.local>
2019-06-18 15:28:21 -0700

    kcuf

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,4 +1,12 @@
 from django.forms import ModelForm
-from .models import Comment
+from .models import Meeting, Comment
 
+class MeetingForm(ModelForm):
+    class Meta:
+        model = Meeting
+        fields = ['date', 'location', 'note']
 
+class CommentForm(ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content', 'date']
--- main_app/views.py
+++ main_app/views.py
@@ -5,8 +5,15 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth import login
-from .models import Profile, Character, Game, User, Comment
+from django.views.generic.edit import CreateView
+from .models import Profile, Meeting, Character, Game, Proflie_photo, Game_photo, Character_photo, Character_sheet_photo,Comment
+from .forms import MeetingForm
+from .forms import CommentForm
+import uuid
+import boto3
 
+S3_BASE_URL = 'https://s3-us-west-1.amazonaws.com/'
+BUCKET = 'taverntalk'
 # Create your views here.
 def home(request):
   return render(request, 'home.html')
@@ -18,6 +25,50 @@ def profile(request):
     characters = Character.objects.all()
     return render(request, 'profile.html',{'games': games, 'characters': characters})
   
+
+def add_profile_photo(request, profile_id):
+    photo_file = request.FILES.get('photo-file', None)
+    if photo_file:
+        s3 = boto3.client('s3')
+        key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
+        try:
+            s3.upload_fileobj(photo_file, BUCKET, key)
+            url = f"{S3_BASE_URL}{BUCKET}/{key}"
+            photo = Proflie_photo(url=url, profile=profile_id)
+            photo.save()
+        except:
+            print('An error occurred uploading file to S3')
+    return redirect('profile', profile=profile_id)
+
+def add_character_photo(request, character_id):
+    photo_file = request.FILES.get('photo-file', None)
+    if photo_file:
+        s3 = boto3.client('s3')
+        key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
+        try:
+            s3.upload_fileobj(photo_file, BUCKET, key)
+            url = f"{S3_BASE_URL}{BUCKET}/{key}"
+            photo = Proflie_photo(url=url, character=character_id)
+            photo.save()
+        except:
+            print('An error occurred uploading file to S3')
+    return redirect('characters_detail', character_id=character_id)
+
+
+def add_game_photo(request, game_id):
+    photo_file = request.FILES.get('photo-file', None)
+    if photo_file:
+        s3 = boto3.client('s3')
+        key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
+        try:
+            s3.upload_fileobj(photo_file, BUCKET, key)
+            url = f"{S3_BASE_URL}{BUCKET}/{key}"
+            photo = Proflie_photo(url=url, game=game_id)
+            photo.save()
+        except:
+            print('An error occurred uploading file to S3')
+    return redirect('games_detail', game_id=game_id)
+
 def signup(request):
     error_message = ''
     if request.method == 'POST':
@@ -43,8 +94,25 @@ def characters_detail(request, character_id):
 def games_detail(request, game_id):
     game = Game.objects.get(id=game_id)
     comment = Comment.objects.all()
-    return render(request, 'games/detail.html', {'game': game, 'comment': comment})
+    comment_form = CommentForm()
+    meetings = Meeting.objects.filter(game=game.id)
+    meeting_form = MeetingForm()
+    return render(request, 'games/detail.html', {'game': game, 'meeting_form': meeting_form, 'meetings': meetings, 'comment_form': comment_form, 'comment':comment})
 
+def add_meeting(request, game_id):
+    form = MeetingForm(request.POST)
+    if form.is_valid():
+        new_meeting = form.save(commit=False)
+        new_meeting.game_id = game_id
+        new_meeting.save()
+    return redirect('games_detail', game_id=game_id)
+def add_comment(request, game_id):
+    form = CommentForm(request.POST)
+    if form.is_valid():
+        new_comment = form.save(commit=False)
+        new_comment.game_id = game_id
+        new_comment.save()
+    return redirect('games_detail', game_id=game_id)
 
 class CharacterCreate(LoginRequiredMixin, CreateView):
     model = Character
