687a1b52a529302eae3565204d5811635d3206f3
Lyndsay Ramberg <lyndsayramberg@Lyndsays-MacBook-Pro.local>
2019-06-18 12:50:48 -0700

    meeting form

--- main_app/urls.py
+++ main_app/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('characters/<int:pk>/update/', views.CharacterUpdate.as_view(), name='characters_update'),
     path('characters/<int:pk>/delete/', views.CharacterDelete.as_view(), name='characters_delete'),
     path('profile/games/<int:game_id>/', views.games_detail, name='games_detail'),
+    path('profile/games/<int:game_id>/add_meeting/', views.add_meeting, name='add_meeting'),
     path('games/create/', views.GameCreate.as_view(), name='games_create'),
     path('games/<int:pk>/update/', views.GameUpdate.as_view(), name='games_update'),
     path('games/<int:pk>/delete/', views.GameDelete.as_view(), name='games_delete'),
